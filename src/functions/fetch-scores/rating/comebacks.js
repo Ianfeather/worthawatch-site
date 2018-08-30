b64226169f7a12bc16dcc5274de647719e31d043
Ian Feather <info@ianfeather.co.uk>
2018-08-30 09:04:09 +0100

    lint

--- src/functions/fetch-scores/rating/comebacks.js
+++ src/functions/fetch-scores/rating/comebacks.js
@@ -1,27 +1,29 @@
-module.exports = function(event) {
+module.exports = function (event) {
+  // This is naive because we only have quarter scores
+  // The best way would be a biggest lead stat
 
-  let winningTeam = event.home_points_scored > event.away_points_scored ? 'home' : 'away';
-  let losingTeam = event.home_points_scored > event.away_points_scored ? 'away' : 'home';
+  let winningTeam = event.home_points_scored > event.away_points_scored ? 'home' : 'away'
+  let losingTeam = event.home_points_scored > event.away_points_scored ? 'away' : 'home'
 
   let quarterDiffs = event[`${winningTeam}_period_scores`].map((score, index) => {
-    return score - event[`${losingTeam}_period_scores`][index];
-  });
+    return score - event[`${losingTeam}_period_scores`][index]
+  })
 
   // If the team was down by 30 at half time
   if (quarterDiffs[1] < -29) {
-    return 1.1;
+    return 1.1
   }
 
   // If the team was down by 20 at the end of the 3rd
   if (quarterDiffs[2] < -19) {
-    return 1.1;
+    return 1.1
   }
 
   // If the team was down by 20 at half time
   if (quarterDiffs[1] < -19) {
-    return 1.05;
+    return 1.05
   }
 
-  return 1;
+  return 1
 
 }
--- test/functions/fetch-scores/rating.test.js
+++ test/functions/fetch-scores/rating.test.js
@@ -1,97 +1,95 @@
-const assert = require('assert');
-const expect = require('chai').expect;
-const createEvent = require('./utils').createEvent;
-const closeness = require('../../../src/functions/fetch-scores/rating/closeness');
+/* global describe, it */
 
-const scores = (home, away) => closeness(createEvent(home, away));
+const expect = require('chai').expect
+const createEvent = require('./utils').createEvent
+const closeness = require('../../../src/functions/fetch-scores/rating/closeness')
+
+const scores = (home, away) => closeness(createEvent(home, away))
 
 describe('Closeness Rating', () => {
-
   describe('Total Scores', () => {
     it('1 point', () => {
-      expect(scores(84, 85)).to.equal(80);
-    });
+      expect(scores(84, 85)).to.equal(80)
+    })
 
     it('2 point', () => {
-      expect(scores(84, 86)).to.equal(80);
-    });
+      expect(scores(84, 86)).to.equal(80)
+    })
 
     it('3 point', () => {
-      expect(scores(84, 87)).to.equal(70);
-    });
+      expect(scores(84, 87)).to.equal(80)
+    })
 
     it('10 point', () => {
-      expect(scores(84, 94)).to.equal(66);
-    });
+      expect(scores(84, 94)).to.equal(66)
+    })
 
     it('20 point', () => {
-      expect(scores(84, 104)).to.equal(52);
-    });
-  });
+      expect(scores(84, 104)).to.equal(52)
+    })
+  })
 
   describe('Comebacks', () => {
     it('with no comeback', () => {
-      expect(scores([25, 25, 25, 25], [25, 25, 25, 26])).to.equal(80);
-    });
+      expect(scores([25, 25, 25, 25], [25, 25, 25, 26])).to.equal(80)
+    })
 
     it('down 20 at the half, close finish', () => {
-      expect(scores([25, 25, 25, 25], [25, 0, 50, 26])).to.equal(84);
-    });
+      expect(scores([25, 25, 25, 25], [25, 0, 50, 26])).to.equal(84)
+    })
 
     it('down 30 at the half, close finish', () => {
-      expect(scores([25, 30, 25, 25], [25, 0, 50, 31])).to.equal(88);
-    });
+      expect(scores([25, 30, 25, 25], [25, 0, 50, 31])).to.equal(88)
+    })
 
     it('down 20 after three, close finish', () => {
-      expect(scores([25, 25, 25, 25], [25, 25, 0, 51])).to.equal(88);
-    });
+      expect(scores([25, 25, 25, 25], [25, 25, 0, 51])).to.equal(88)
+    })
 
     it('down 20 after three, win by 5', () => {
-      expect(scores([25, 25, 25, 25], [25, 25, 0, 55])).to.equal(77);
-    });
+      expect(scores([25, 25, 25, 25], [25, 25, 0, 55])).to.equal(77)
+    })
 
     it('down 20 after three, win by 20 (should be higher!!)', () => {
-      expect(scores([25, 25, 25, 25], [25, 25, 0, 70])).to.equal(57);
-    });
-  });
+      expect(scores([25, 25, 25, 25], [25, 25, 0, 70])).to.equal(57)
+    })
+  })
 
   describe('Overtimes', () => {
     it('with no overtime', () => {
-      expect(scores([25, 25, 25, 25], [25, 25, 25, 26])).to.equal(80);
-    });
+      expect(scores([25, 25, 25, 25], [25, 25, 25, 26])).to.equal(80)
+    })
 
     it('one overtime', () => {
-      expect(scores([25, 25, 25, 25, 25], [25, 25, 25, 25, 26])).to.equal(83);
-    });
+      expect(scores([25, 25, 25, 25, 25], [25, 25, 25, 25, 26])).to.equal(83)
+    })
 
     it('one overtime + comeback', () => {
-      expect(scores([25, 0, 50, 25, 25], [25, 25, 25, 25, 26])).to.equal(91);
-    });
+      expect(scores([25, 0, 50, 25, 25], [25, 25, 25, 25, 26])).to.equal(91)
+    })
 
     it('two overtimes', () => {
-      expect(scores([25, 25, 25, 25, 25, 25], [25, 25, 25, 25, 25, 26])).to.equal(86);
-    });
+      expect(scores([25, 25, 25, 25, 25, 25], [25, 25, 25, 25, 25, 26])).to.equal(86)
+    })
 
     it('two overtimes + comeback', () => {
-      expect(scores([25, 0, 50, 25, 25, 25], [25, 25, 25, 25, 25, 26])).to.equal(95);
-    });
+      expect(scores([25, 0, 50, 25, 25, 25], [25, 25, 25, 25, 25, 26])).to.equal(95)
+    })
 
     it('three overtimes', () => {
-      expect(scores([25, 25, 25, 25, 25, 25, 25], [25, 25, 25, 25, 25, 25, 26])).to.equal(89);
-    });
+      expect(scores([25, 25, 25, 25, 25, 25, 25], [25, 25, 25, 25, 25, 25, 26])).to.equal(89)
+    })
 
     it('three overtimes + comeback', () => {
-      expect(scores([25, 0, 50, 25, 25, 25, 25], [25, 25, 25, 25, 25, 25, 26])).to.equal(98);
-    });
+      expect(scores([25, 0, 50, 25, 25, 25, 25], [25, 25, 25, 25, 25, 25, 26])).to.equal(98)
+    })
 
     it('four overtimes', () => {
-      expect(scores([25, 25, 25, 25, 25, 25, 25, 25], [25, 25, 25, 25, 25, 25, 25, 26])).to.equal(93);
-    });
+      expect(scores([25, 25, 25, 25, 25, 25, 25, 25], [25, 25, 25, 25, 25, 25, 25, 26])).to.equal(93)
+    })
 
     it('four overtimes + comeback', () => {
-      expect(scores([25, 0, 50, 25, 25, 25, 25, 25], [25, 25, 25, 25, 25, 25, 25, 26])).to.equal(100);
-    });
-
-  });
-
-});
+      expect(scores([25, 0, 50, 25, 25, 25, 25, 25], [25, 25, 25, 25, 25, 25, 25, 26])).to.equal(100)
+    })
+  })
+})
