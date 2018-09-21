0a9b34740763622a78ae59fcc1ed425ee378a7d4
Ian Feather <info@ianfeather.co.uk>
2018-09-21 19:19:28 +0100

    lint

--- src/site/index.js
+++ src/site/index.js
@@ -1,10 +1,4 @@
-/* global window */
-
 import './style'
 import App from './components/app'
 
 export default App
-
-window.addEventListener('beforeinstallprompt', () => {
-  console.log('BEFORE INSTALLLLLL')
-})
