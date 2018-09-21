bbd76f120a99e30ae643c48f7debc20db81fa8d4
Ian Feather <info@ianfeather.co.uk>
2018-09-21 23:13:23 +0100

    deps

--- src/functions/fetch-scores/lib.js
+++ src/functions/fetch-scores/lib.js
@@ -2,6 +2,8 @@ const AWS = require('aws-sdk')
 const dynamodb = new AWS.DynamoDB.DocumentClient()
 const prepareGameData = require('./prepare-game-data')
 
+AWS.config.update({region: 'us-east-1'})
+
 module.exports = {
   getScores: function (event, cb) {
     var getParams = {
