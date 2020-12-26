a9a9dc3f35c4b8b871f9e73ff05e57f1965c6605
Ian Feather <info@ianfeather.co.uk>
2020-12-26 15:56:34 +0000

    maintenance

--- src/site/components/header/index.js
+++ src/site/components/header/index.js
@@ -6,9 +6,7 @@ const Header = () => (
     <div class='flex center flex-column header'>
       <h1 class={style.title}>Worth a watch?</h1>
       <div class={style.subtitle}>
-        <p class={style.quote}>Then there’s the absolute degenerates, those who fell in love with a city or a team and have to navigate five-hour (or more) time differences to keep up with early games tipping off after midnight their time.</p>
-        <a href='https://theathletic.com/211607/2018/01/20/trusting-the-process-from-across-the-atlantic-ocean/' class={style.attribution}>Derek Bodner, The Athletic</a>
-        {/* There are many different classifications of NBA fans overseas */}
+        <p class={style.quote}>Find out if yesterday's NBA games are worth catching up with on League Pass.</p>
       </div>
     </div>
     <div class='flex flex-column' />
--- src/site/components/no-results/index.js
+++ src/site/components/no-results/index.js
@@ -4,7 +4,7 @@ export default class NoResults extends Component {
   render () {
     return (
       <div className='no-results'>
-        <h1 className='center flex'>There were no games yesterday :(</h1>
+        <h1 className='center flex'>Sorry, this is currently under maintenance as I find a new data source. Hope to be back soon!</h1>
       </div>
     )
   }
