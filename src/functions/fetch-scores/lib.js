f9a6b376c618d6fbbdc7c23b6270f519582d42fd
Ian Feather <info@ianfeather.co.uk>
2022-03-23 15:36:16 +0100

    debug

--- src/functions/fetch-scores/lib.js
+++ src/functions/fetch-scores/lib.js
@@ -3,11 +3,14 @@ const prepareGameData = require('./prepare-game-data')
 
 module.exports = {
   getScores: function (event, cb) {
+    console.log("getScores - called");
     NBA.stats.scoreboard({gameDate: event.queryStringParameters.date})
       .then(dailyScores => {
+        console.log("getScores - received");
         const data = {
           games: prepareGameData(dailyScores)
         }
+        console.log("getScores - prepared data");
 
         return cb(null, data)
       })
